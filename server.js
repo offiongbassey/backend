53a2c890a2d252ac6f2e787daf65b8f17bdce3b3
naijaspeed1 <naijaspeed1@gmail.com>
2023-06-05 18:49:23 +0100

    deploying react to render

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://school-forum-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://unicross-forum.onrender.com"],
     credentials: true
 }));
 
